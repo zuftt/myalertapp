8ba0068d7a2847c513dd9dac9f58679b7681989a
zuftt <112378668+zuftt@users.noreply.github.com>
2024-11-12 04:40:55 +0800

    1st

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,51 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Tabs } from 'expo-router'
+import { Ionicons,AntDesign,MaterialCommunityIcons,FontAwesome } from '@expo/vector-icons'
+
+export default function TabLayout() {
+  return (
+    <Tabs
+    screenOptions={{
+      tabBarActiveTintColor:'black'
+    }}>
+        <Tabs.Screen name='home'
+        options={{
+            title:'Home',
+            headerShown: false,
+            tabBarIcon:({black})=><Ionicons name="home-sharp" size={24} color="black" />
+          }}
+          />
+
+          
+
+        <Tabs.Screen 
+        options={{
+          title:'Reports',
+            headerShown: false,
+            tabBarIcon:({black})=><FontAwesome name="file-text" size={24} color="black" />
+        }}
+        name='reports'/>
+
+<Tabs.Screen 
+        options={{
+          title:'Maps',
+            headerShown: false,
+            tabBarIcon:({black})=><MaterialCommunityIcons name="google-maps" size={24} color="black" />
+        }}
+        name='maps'/>
+
+        <Tabs.Screen 
+        options={{
+          headerShown: false,
+          tabBarIcon:({black})=><Ionicons name="file-tray" size={24} color="black" />
+        }}name='inbox'/>
+        
+        <Tabs.Screen 
+        options={{
+          headerShown: false,
+        }}
+        name='profile'/>
+    </Tabs>
+  )
+}
---
+++ components/Home/Slider.jsx
@@ -0,0 +1,58 @@
+import { View, StyleSheet, Image, Dimensions } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from './../../app/config/FirebaseConfig';
+import { FlatList } from 'react-native';
+
+export default function Slider() {
+  const [sliderList, setSliderList] = useState([]);
+
+  useEffect(() => {
+    GetSliders();
+  }, []);
+
+  const GetSliders = async () => {
+    setSliderList([]);
+    try {
+      const snapshot = await getDocs(collection(db, 'Slider'));
+      const sliders = [];
+      snapshot.forEach((doc) => {
+        sliders.push(doc.data());
+      });
+      setSliderList(sliders);
+    } catch (error) {
+      console.error('Error fetching slider data: ', error);
+    }
+  };
+
+  return (
+    <View>
+      <FlatList
+      horizontal={true}
+      showsHorizontalScrollIndicator={false}
+        data={sliderList}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) => (
+          <View style={styles.imageContainer}>
+            <Image source={{ uri: item.imageurl }} style={styles.sliderImage} />
+          </View>
+        )}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  imageContainer: {
+    width: Dimensions.get('screen').width * 0.9,
+    height: 160,
+    borderRadius: 15,
+    overflow: 'hidden',
+    marginVertical: 10,
+    alignSelf: 'center',
+  },
+  sliderImage: {
+    width: '100%',
+    height: '100%',
+  },
+});
