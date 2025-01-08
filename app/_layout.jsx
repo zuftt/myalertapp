742ccd94b6d43d743dd109487dafb68528b40a3d
Imran Afzal Pansota <111197710+imranAfzal43A@users.noreply.github.com>
2025-01-08 12:48:10 +0500

    implemented push notifications

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,11 +1,19 @@
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
-import { ClerkProvider } from '@clerk/clerk-expo';
-import { Slot } from 'expo-router';
-import * as SecureStore from 'expo-secure-store';
-import { useState } from "react";
-import { Text } from "react-native";
-
+import { ClerkProvider } from "@clerk/clerk-expo";
+import * as SecureStore from "expo-secure-store";
+import { Alert, Text } from "react-native";
+import * as Notifications from "expo-notifications";
+import { useEffect } from "react";
+import { registerForPushNotificationsAsync } from "@/components/notifications";
+import * as Clipboard from "expo-clipboard";
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+  }),
+});
 
 // Token cache for Clerk
 const tokenCache = {
@@ -15,11 +23,11 @@ const tokenCache = {
       if (item) {
         console.log(`${key} was used 🔐`);
       } else {
-        console.log('No values stored under key: ' + key);
+        console.log("No values stored under key: " + key);
       }
       return item;
     } catch (error) {
-      console.error('SecureStore get item error: ', error);
+      console.error("SecureStore get item error: ", error);
       await SecureStore.deleteItemAsync(key);
       return null;
     }
@@ -28,7 +36,7 @@ const tokenCache = {
     try {
       return SecureStore.setItemAsync(key, value);
     } catch (err) {
-      console.error('SecureStore set item error: ', err);
+      console.error("SecureStore set item error: ", err);
     }
   },
 };
@@ -38,12 +46,26 @@ const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
 
 export default function RootLayout() {
   const [fontsLoaded] = useFonts({
-    'outfit': require('./../assets/fonts/Outfit-Regular.ttf'),
-    'outfit-medium': require('./../assets/fonts/Outfit-Medium.ttf'),
-    'outfit-bold': require('./../assets/fonts/Outfit-Bold.ttf'),
+    outfit: require("./../assets/fonts/Outfit-Regular.ttf"),
+    "outfit-medium": require("./../assets/fonts/Outfit-Medium.ttf"),
+    "outfit-bold": require("./../assets/fonts/Outfit-Bold.ttf"),
   });
 
   // Show loading message until fonts are fully loaded
+  useEffect(() => {
+    registerForPushNotificationsAsync().then((token) => {
+      console.log("token :", token);
+      Alert.alert(
+        "Push Notification Token",
+        `Your token is: ${token}`,
+        [
+          { text: "Copy Token", onPress: () => Clipboard.setString(token) },
+          { text: "OK", onPress: () => console.log("Alert dismissed") },
+        ],
+        { cancelable: true }
+      );
+    });
+  }, []);
   if (!fontsLoaded) {
     return <Text>Loading...</Text>;
   }
@@ -67,33 +89,28 @@ export default function RootLayout() {
         <Stack.Screen
           name="new_report/index"
           options={{
-            title:'Make a new report',
-            
+            title: "Make a new report",
           }}
         />
         <Stack.Screen
           name="feedback/index"
           options={{
-            title:'Submit your feedback',
-            
+            title: "Submit your feedback",
           }}
         />
         <Stack.Screen
           name="admin/index"
           options={{
-            title:'Login',
-            
+            title: "Login",
           }}
         />
         <Stack.Screen
           name="admin/create_alert"
           options={{
-            title:'Create an alert',
-            
+            title: "Create an alert",
           }}
         />
       </Stack>
-      
     </ClerkProvider>
   );
 }
